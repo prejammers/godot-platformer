dcb907bdcf196a737dff49f5a99369f7f5bc425f
Phantom0425 <riosjesse17@gmail.com>
2024-06-17 01:09:45 -0500

    pause menu done

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,16 +1,18 @@
 extends Node2D
 
-func game_over():
-	pass
-
-func new_game():
-	pass
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
+@onready var pause_menu = $Player/Camera2D2/Pause_Menu
+var paused = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("Pause"):
+		pauseMenu()
+		
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+	paused = !paused
