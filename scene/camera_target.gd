e5f55854cb259966d990e206df7a46135c584cf7
cholman <holman.cody@gmail.com>
2024-06-19 03:21:25 -0500

    camera wip

---
+++ scene/camera_target.gd
@@ -0,0 +1,12 @@
+extends MeshInstance2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var player = $".."
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	if player.horizontal_direction > 1:
+		move_local_x(+10)
