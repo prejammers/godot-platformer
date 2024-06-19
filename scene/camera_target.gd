615bb411e5077be1cb20b2dac6f814da2a0ade16
cholman <holman.cody@gmail.com>
2024-06-19 03:31:40 -0500

    fixed crash

--- scene/camera_target.gd
+++ scene/camera_target.gd
@@ -7,6 +7,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
-	if player.horizontal_direction > 1:
-		move_local_x(+10)
+	pass
