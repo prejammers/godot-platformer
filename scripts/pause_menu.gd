edc5fe9f00ab387f1b38d97f6c24c3b4e4557e5c
cholman <holman.cody@gmail.com>
2024-06-19 00:42:02 -0500

    jump-test repair v2

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,14 +1,10 @@
 extends Control
 
-@onready var player = $"../.."
+@onready var main = $"../../.."
 
 func _on_resume_pressed():
-<<<<<<< HEAD
-	player.pauseMenu()
-=======
 	$AudioStreamPlayer2D.play()
 	main.pauseMenu()
->>>>>>> main
 
 func _on_quit_pressed():
 	get_tree().quit()
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,8 +3,6 @@ extends CharacterBody2D
 @export var speed = 300
 @export var gravity = 30
 @export var jump_force = 1000
-@onready var pause_menu = $Camera2D/Pause_Menu
-var paused = false
 
 func _physics_process(delta):
 	if !is_on_floor():
@@ -18,16 +16,3 @@ func _physics_process(delta):
 	var horizontal_direction = Input.get_axis("move_left", "move_right")
 	velocity.x = speed * horizontal_direction
 	move_and_slide()
-
-func _process(delta):
-	if Input.is_action_just_pressed("Pause"):
-		pauseMenu()
-		
-func pauseMenu():
-	if paused:
-		pause_menu.hide()
-		Engine.time_scale = 1
-	else:
-		pause_menu.show()
-		Engine.time_scale = 0
-	paused = !paused
