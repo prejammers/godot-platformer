9d6530cdb1be953eb22c3cb05930ff0976312599
Phantom <riosjesse17@gmail.com>
2024-06-19 03:33:56 -0500

    level select sorta

--- scene/level_select.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-@onready var Level_Select = $"../.."
-
-func _on_resume_pressed():
-	$AudioStreamPlayer2D.play()
-	Level_Select.pauseMenu()
-
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,7 +4,9 @@ extends CharacterBody2D
 @export var gravity = 30
 @export var jump_force = 1000
 @onready var pause_menu = $Camera2D/Pause_Menu
+@onready var level_select = $"Camera2D/Level Select"
 var paused = false
+var leveled = false
 
 func _physics_process(delta):
 	if !is_on_floor():
@@ -30,3 +32,15 @@ func pauseMenu():
 		pause_menu.show()
 		Engine.time_scale = 0
 	paused = !paused
+	
+	if Input.is_action_just_pressed("Level Select"):
+		levelSelect()
+
+func levelSelect():
+	if leveled:
+		level_select.hide()
+		Engine.time_scale = 1
+	else:
+		level_select.show()
+		Engine.time_scale = 0
+	leveled = !leveled
