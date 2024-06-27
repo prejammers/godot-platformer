94b54b294bcafa1ec77a8e04cce9e188cf428d79
cholman <holman.cody@gmail.com>
2024-06-27 02:44:32 -0500

    more adjustments

---
+++ scene/level_blocks/AudioStreamPlayer2D.gd
@@ -0,0 +1,12 @@
+extends AudioStreamPlayer2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#self.play()
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
